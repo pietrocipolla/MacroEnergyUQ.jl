9b529fd86041ed14e2e360427d1b012dc8cf6095
pietrocipolla <leonardo.chiani@polimi.it>
2025-10-22 10:56:22 -0400

    added sensitivity functions to export

--- src/MacroEnergyUQ.jl
+++ src/MacroEnergyUQ.jl
@@ -11,6 +11,6 @@ include("process_mc_data.jl")
 include("run_mc.jl")
 include("ot_indices.jl")
 
-export process_mc_data, run_mc
+export process_mc_data, run_mc, ot_indices, ot_indices_wb
 
 end # module MacroEnergyUQ
